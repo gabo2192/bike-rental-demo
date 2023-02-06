aa31434b9a11fd3586c42432a579178b507ce7cc
Gabriel Rojas <gabriel.rojasa21@gmail.com>
2023-02-06 01:12:43 -0500

    fix: remove static bike info

--- src/data/bikes.ts
+++ src/data/bikes.ts
@@ -6,30 +6,6 @@ const supabase = createClient(
   process.env.NEXT_APP_PUBLIC_SUPABASE_KEY as string
 );
 
-const bikes: Bike[] = [
-  {
-    name: "Electric Bike",
-    type: "electric",
-    image:
-      "https://res.cloudinary.com/dbravos/image/upload/v1675660530/bike-rental/e-bike_kswvlg.jpg",
-    slug: "/e-bike",
-  },
-  {
-    name: "Traditional Bike",
-    type: "normal",
-    image:
-      "https://res.cloudinary.com/dbravos/image/upload/v1675660531/bike-rental/bike_bpgozl.jpg",
-    slug: "/bike",
-  },
-  {
-    name: "Vintage Bike",
-    type: "old",
-    image:
-      "https://res.cloudinary.com/dbravos/image/upload/v1675660531/bike-rental/vintage_wmhcy7.jpg",
-    slug: "/v-bike",
-  },
-];
-
 const getBikes = async () => {
   let { data: bikes, error } = await supabase.from("bikes").select("*");
   if (error) {
